4568a3d6f60bdc5ff2ca818cc43700ad5023e912
Jeff Lloyd <jeffwelcome@icloud.com>
2019-07-17 09:24:38 +0100

    modified cart-dropdown and checkout components also modified cart selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -17,3 +17,10 @@ export const selectCartItemsCount = createSelector(
     cartItems =>
     cartItems.reduce((accumaletedQuantity, cartItem) => accumaletedQuantity + cartItem.quantity, 0)
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+    cartItems.reduce((accumaletedQuantity, cartItem) => accumaletedQuantity + cartItem.quantity * cartItem.price, 0)
+
+)
